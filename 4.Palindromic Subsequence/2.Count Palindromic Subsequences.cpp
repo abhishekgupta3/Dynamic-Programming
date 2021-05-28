02b7ebb796f09c03be507298327a8c3238d0f752
abhishekgupta3 <abhishek_1901ee03@iitp.ac.in>
2021-05-28 15:32:06 +0530

    cnt palindromic subsequence

--- 4.Palindromic Subsequence/2.Count Palindromic Subsequences.cpp
+++ 4.Palindromic Subsequence/2.Count Palindromic Subsequences.cpp
@@ -4,27 +4,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cntLPS(string a, string b, int n, int m) {
-	if (m == 0 || n == 0)return 0;
-	if (a[n - 1] == b[m - 1])return 1 + cntLPS(a, b, n - 1, m - 1);
+// Recursive
+int cntPS(string s, int i, int j) {
+	if (i == j)return 1;
+	if (i > j)return 0;
+	if (s[i] == s[j])return 1 + cntPS(s, i, j - 1) + cntPS(s, i + 1, j);
 
-	return cntLPS(a, b, n - 1, m) + cntLPS(a, b, n, m - 1);
+	return cntPS(s, i, j - 1) + cntPS(s, i + 1, j) - cntPS(s, i + 1, j - 1);
 }
 
 int main() {
-
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
 
 	string s; cin >> s;
-	string r = s;
-	reverse(s.begin(), s.end());
+	int n = s.length();
 
-	cout << cntLPS(r, s, r.length(), s.length()) << '\n';
+	// Bottom up: T.C: O(N*N) A.S: O(N*N)
+	int dp[n + 1][n + 1];
+	memset(dp, 0, sizeof dp);
 
+	// filling diagonal
+	for (int i = 0; i < n; ++i)dp[i][i] = 1;
+
+	for (int k = 2; k <= n; ++k) {
+		for (int i = 0; i <= n - k; ++i)	{
+			int j = k + i - 1;
+			if (s[i] == s[j]) dp[i][j] = 1 + dp[i + 1][j] + dp[i][j - 1];
+			else dp[i][j] = dp[i + 1][j] + dp[i][j - 1] - dp[i + 1][j - 1];
+		}
+	}
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			cout << dp[i][j] << " ";
+		}
+		cout << '\n';
+	}
+
+	cout << dp[0][n - 1] << '\n';
 
 	return 0;
-
 }
